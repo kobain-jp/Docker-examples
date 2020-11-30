f1be6c30aa76fd23b8c0c5537c286d60076a57c2
yoshifumi <yoshifumi@kobain.local>
2020-11-30 22:51:45 +0900

    add springboot example

---
+++ springboot-production-example/README.md
@@ -0,0 +1,15 @@
+### setup
+
+Spring Boot Extension Pack
+Java Extension Pack
+
+### 
+
+1.create project via spring initializer
+
+2.add controller
+
+
+3.create Dockerfile .dockerignore
+
+4.add description to docker-compose.yml
---
+++ springboot-production-example/src/main/java/jp/kobain/springbootproductionexample/SpringbootProductionExampleApplication.java
@@ -0,0 +1,13 @@
+package jp.kobain.springbootproductionexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProductionExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProductionExampleApplication.class, args);
+	}
+
+}
